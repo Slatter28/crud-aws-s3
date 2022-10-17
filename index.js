4723085fc986182e3ebbf7c1b781d23198c37f11
Roger Matamoros <88455490+Slatter28@users.noreply.github.com>
2022-10-16 19:59:17 -0500

    extructura hecha

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fileUpload = require('express-fileupload')
-const {uploadFile, getFiles, getFileURL, downloadFile}= require('./s3')
-
+const filesRoutes=require('./routes/files')
 
 const app = express();
 
@@ -16,30 +15,7 @@ app.get('/',(req,res)=>{
     res.status(200).json({message:'Welcome to server'})
 })
 
-app.post('/files',async (req,res)=>{
-    const result = await uploadFile(req.files.file);
-    res.status(200).json({result})
-})
-
-
-app.get('/files', async (req, res) => {
-    const result = await getFiles()
-    res.json(result.Contents)
-})
-
-app.get('/files/:fileName', async (req, res) => {
-    const result = await getFileURL(req.params.fileName)
-    res.json({
-        url: result
-    })
-})
-
-app.get('/downloadfile/:fileName', async (req, res) => {
-    await downloadFile(req.params.fileName)
-    res.json({message: "archivo descargado"})
-})
-
-
+app.use('/files',filesRoutes);
 
 app.use(express.static('images'))
 
